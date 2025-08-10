da7e7483ed9341d8019967e264dfcf6a5a774ff6
Daniel Fernandez <mdanif9728@gmail.com>
2025-08-10 09:33:24 -0700

    I have redesign the hero-section making it full-responsive

--- app/page.tsx
+++ app/page.tsx
@@ -1,67 +1,90 @@
+"use client";
+
 import { Link } from "@heroui/link";
-import { Snippet } from "@heroui/snippet";
-import { Code } from "@heroui/code";
-import { Colors } from "@/config/colors";
 import { button as buttonStyles } from "@heroui/theme";
+import { useMediaQuery } from "react-responsive";
 
 import { siteConfig } from "@/config/site";
 import { title, subtitle } from "@/components/primitives";
 import { GithubIcon } from "@/components/icons";
+import { CircleCheckIn } from "@/components/icons";
+import { LocationMap } from "@/components/icons";
 import Image from "next/image";
 
 export default function Home() {
+  const isMobile = useMediaQuery({ maxWidth: 768 });
+
   return (
-    <main className="grid grid-cols-1 md:grid-cols-2 gap-4 p-4 " style={{ backgroundColor: Colors.background }}>
+    <main className="grid grid-cols-1 lg:grid-cols-2 lg:gap-4 p-4 -mt-16 bg-background">
       {/* Left */}
-      <section className="flex flex-col items-start justify-center gap-4 py-8 md:py-10 md:pl-16">
+      <section className="flex flex-col items-start justify-center gap-4 py-8 md:py-10 md:pl-10">
         <div className="inline-block max-w-xl justify-center">
-          <span className={title({class: ""})}>
-            The Digital Face of Your Restaurant.
+          <span
+            className={title({
+              size: isMobile ? "sm" : "lg",
+              class: "text-primary",
+            })}
+          >
+            The Digital Face of <br /> Your Restaurant.
           </span>
 
-          <div className={subtitle({ class: "mt-4" })}>
+          <div className={subtitle({ class: "mt-4 hidden md:inline-block" })}>
             Transform your restaurant's online presence with professional
             photography, optimized menus, and a website that converts visitors
             into customers.
           </div>
+
+          <div className="inline-block md:hidden max-w-xl items-center justify-center ">
+            <Image
+              src={"/hero-img-corp.png"}
+              alt="Hero Image"
+              className="rounded-lg border-4"
+              priority
+              width={500}
+              height={500}
+            />
+          </div>
         </div>
 
-        <div className="flex gap-3">
+        <div className="flex flex-col lg:flex-row gap-2 font-small ">
+          <span className="text-gray-600 flex gap-2">
+            {" "}
+            <CircleCheckIn className="text-success" /> Professional Website
+          </span>
+          <span className="text-gray-600 flex gap-2">
+            {" "}
+            <CircleCheckIn className="text-success" /> Google Optimization
+          </span>
+          <span className="text-gray-600 flex gap-2">
+            {" "}
+            <CircleCheckIn className="text-success" /> Lifetime Support
+          </span>
+        </div>
+
+        <div className="flex flex-col md:flex-row gap-4">
           <Link
             isExternal
-            className={buttonStyles({
+            className={`${buttonStyles({
               color: "primary",
               radius: "full",
-              variant: "shadow",
-            })}
+              size: "lg",
+            })} px-8 bg-secondary text-primary`}
             href={siteConfig.links.docs}
           >
-            Documentation
+            Get Started
           </Link>
-          <Link
-            isExternal
-            className={buttonStyles({ variant: "bordered", radius: "full" })}
-            href={siteConfig.links.github}
-          >
-            <GithubIcon size={20} />
-            GitHub
-          </Link>
-        </div>
-
-        <div className="mt-8">
-          <Snippet hideCopyButton hideSymbol variant="bordered">
-            <span>
-              Get started by editing <Code color="primary">app/page.tsx</Code>
-            </span>
-          </Snippet>
+          <span className="text-gray-600 flex items-center gap-1">
+            <span className="hidden md:inline-block">{"-->"}</span> Get your
+            free consultation today
+          </span>
         </div>
       </section>
 
       {/* Right */}
-      <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+      <section className="hidden lg:flex flex-col items-center justify-center gap-4 py-8 md:py-10">
         <div className="inline-block max-w-xl items-center justify-center">
           <Image
-            src={"/hero-img.png"}
+            src={"/hero-img-corp.png"}
             alt="Hero Image"
             className="rounded-lg border-4"
             priority
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
-import {heroui} from "@heroui/theme"
+import { heroui } from "@heroui/theme"
 
 /** @type {import('tailwindcss').Config} */
 const config = {
@@ -12,6 +12,16 @@ const config = {
       fontFamily: {
         sans: ["var(--font-sans)"],
         mono: ["var(--font-mono)"],
+      },
+      colors: {
+        primary: "#122243", // Dark Blue
+        secondary: "#FBBF24", // Yellow
+        accent: "#38BDF8", // Light Blue
+        background: "#F7F5F2", // Light Gray 
+        success: "#2A8703", // Green
+        warning: "#F59E0B", // Orange
+        error: "#DC2626", // Red
+        info: "#3B82F6", // Blue
       },
     },
   },
